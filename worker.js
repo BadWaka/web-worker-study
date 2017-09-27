167c28f55a8f056e203fa001fdbe11acb3661367
BadWaka <1456683844@qq.com>
2017-09-27 16:19:56 +0800

    worker作用域

--- worker.js
+++ worker.js
@@ -1,4 +1,6 @@
 // worker.js
+// 注意： 在主线程中使用时，onmessage和postMessage() 必须挂在worker对象上，
+// 而在worker中使用时不用这样做。原因是，在worker内部，worker是有效的全局作用域
 onmessage = function (p1) {
     console.log('主线程传进来的参数', p1);
     postMessage(['111', {c: 1, d: 2}]);
